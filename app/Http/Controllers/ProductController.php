4dc2ab50304a5ea5041affe4e62a81157040c9a9
jshainesantos <jessicashainesantos@gmail.com>
2024-05-07 19:39:46 +0800

    added 'create a product' page with validator

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -3,7 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-
+use Illuminate\Support\Facades\Validator;
 class ProductController extends Controller
 {
     //show product page
@@ -17,8 +17,17 @@ class ProductController extends Controller
     }
 
     //will store a product in db
-    public function store() {
+    public function store(Request $request) {
+        $rules = [
+            'name'=> 'required|min:5',
+            'price'=> 'required|numeric',
+        ];
 
+        $validator = Validator::make($request->all(),$rules);
+
+        if($validator->fails()){
+            return redirect()->route('products.create')->withInput()->withErrors($validator);
+        }
     }
 
     //show edit product page
