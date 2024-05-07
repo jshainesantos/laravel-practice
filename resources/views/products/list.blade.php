3dc427f83216704ecb664eb493157aa64c059822
jshainesantos <jessicashainesantos@gmail.com>
2024-05-07 21:40:25 +0800

    added icons adjusted responsiveness

--- resources/views/products/list.blade.php
+++ resources/views/products/list.blade.php
@@ -3,8 +3,9 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Create a Product</title>
+    <title>Products</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
 </head>
 
 <body class="bg-secondary">
@@ -26,44 +27,47 @@
             </div>
         </div>
 
-        <h1>List of products 📦</h1>
-        <table class="table mt-5 text-center">
-            <thead>
-            <tr>
-                <th scope="col">ID</th>
-                <th scope="col">Image</th>
-                <th scope="col">Name</th>
-                <th scope="col">Price</th>
-                <th scope="col">Description</th>
-                <th scope="col">Created at</th>
-                <th scope="col">Action</th>
-            </tr>
-            </thead>
-            <tbody class="table-group-divider">
-            @if ($products->isNotEmpty())
-                @foreach ($products as  $product)
-                    <tr>
-                        <th scope="row">{{ $product->id }}</th>
-                        <td>
-                            @if ( $product->image != "")
-                            <img width="70" src="{{ asset('uploads/products/' . $product->image)}}" alt="">
-                            @endif    
-
-                        </td>
-                        <td>{{ $product->name }}</td>
-                        <td>PHP {{ $product->price }}</td>
-                        <td>{{ $product->description }}</td>
-                        <td>{{ \Carbon\Carbon::parse($product->created_at)->format('d M, Y') }}</td>
-                        <td>
-                            <a href="" class="btn btn-secondary">Edit</a>
-                            <a href="" class="btn btn-danger">Delete</a>
-                        </td>
-                    </tr>
-                @endforeach
-            @endif
-            </tbody>
-        </table>
-        <hr>
+        <div class="container">
+            <h1 class="text-center mb-5 mt-1">List of products 📦</h1>
+                <div class="table-responsive">
+                <table class="table table-striped table-bordered align-middle text-center">
+                    <thead>
+                        <tr>
+                            <th scope="col">ID</th>
+                            <th scope="col">Image</th>
+                            <th scope="col">Name</th>
+                            <th scope="col">Price</th>
+                            <th scope="col">Description</th>
+                            <th scope="col">Created at</th>
+                            <th scope="col">Action</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        @if ($products->isNotEmpty())
+                        @foreach ($products as  $product)
+                        <tr>
+                            <td>{{ $product->id }}</td>
+                            <td>
+                                @if ( $product->image != "")
+                                <img width="70" src="{{ asset('uploads/products/' . $product->image)}}" alt="">
+                                @endif    
+                            </td>
+                            <td>{{ $product->name }}</td>
+                            <td>PHP {{ $product->price }}</td>
+                            <td>{{ $product->description }}</td>
+                            <td>{{ \Carbon\Carbon::parse($product->created_at)->format('d M, Y') }}</td>
+                            <td>
+                                <a href="" class="btn material-icons text-secondary">edit</a>
+                                <a href="" class="btn material-icons text-danger">delete</a>
+                            </td>
+                        </tr>
+                        @endforeach
+                        @endif
+                    </tbody>
+                </table>
+                </div>
+            <hr>
+        </div>
     </div>
 
 </div>
